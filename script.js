2bef05bfc3e35df688ce6a5998b51a9813d48595
Ignacia Silva <ignaciasilva1@gmail.com>
2022-10-29 18:23:44 -0300

    unidad1 completada

--- script.js
+++ script.js
@@ -1,57 +1,32 @@
+import checkComplete from './components/checkComplete.js';
+import deleteIcon from './components/deleteIcon.js';
 
-
-const btn = document.querySelector("[data-form-btn]")
-
+const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[ data-form-iput]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement('li');
-    task.classList.add("card");
-    input.value = '';
-    //backticks 
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  //backticks
+  const taskContent = document.createElement('div');
 
-    const taskContent = document.createElement("div") 
-    taskContent.appendChild(checkComplete()) //checklis
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete()); //checklist
+  taskContent.appendChild(titleTask); //titulo de la tarea
+  // task.innerHTML = content;
 
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task")
-    titleTask.innerText = value
-   taskContent.appendChild(titleTask); //titulo de la tarea
-const content = `
-
-<i class="fas fa-trash-alt trashIcon icon"></i>`
-    //task.innerHTML = content;
-    task.appendChild(taskContent);
-
-    list.appendChild(task);
-   
-    console.log(content);
-}
- 
-
-//Arrow functions o funciones anónimas
-btn.addEventListener('click', createTask); 
-
-
-const checkComplete = () => {  //es el hijo
-const i = document.createElement("i")
-i.classList.add("far", "fa-check-square", "icon") //son los elementos de la clase
-i.addEventListener("click", compleTask);
-return i
-
-}
-
-//Inmediately invoked function expression IIFE
-const compleTask = (event) => { // arrow function
-    const element = event.target;
-    element.classList.toggle("fas"); //sirve para al momento de hacer click se quite el fondo o no del  icono
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
+  list.appendChild(task);
 };
 
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
 
 

b38b4ade1731f5aeb0fbcb08f8ae619269e8eb9a
Ignacia Silva <ignaciasilva1@gmail.com>
2022-10-29 17:27:12 -0300

    clase 04 completada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const btn = document.querySelector("[data-form-btn]")
 
 
@@ -11,12 +13,18 @@ const createTask = (evento) => {
     input.value = '';
     //backticks 
 
-const content = `<div>
-<i class="far fa-check-square icon"></i>
-<span class="task">${value}</span>
-</div>
+    const taskContent = document.createElement("div") 
+    taskContent.appendChild(checkComplete()) //checklis
+
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task")
+    titleTask.innerText = value
+   taskContent.appendChild(titleTask); //titulo de la tarea
+const content = `
+
 <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
 
     list.appendChild(task);
    
@@ -24,8 +32,26 @@ const content = `<div>
 }
  
 
-console.log(btn);
-
 //Arrow functions o funciones anónimas
 btn.addEventListener('click', createTask); 
+
+
+const checkComplete = () => {  //es el hijo
+const i = document.createElement("i")
+i.classList.add("far", "fa-check-square", "icon") //son los elementos de la clase
+i.addEventListener("click", compleTask);
+return i
+
+}
+
+//Inmediately invoked function expression IIFE
+const compleTask = (event) => { // arrow function
+    const element = event.target;
+    element.classList.toggle("fas"); //sirve para al momento de hacer click se quite el fondo o no del  icono
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+
+};
+
+
 
